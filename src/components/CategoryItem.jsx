3aafa0dd7c321d2eaa58af1600c2999a779830af
unknown <piyushkumar4075@gmail.com>
2024-10-11 15:21:29 +0530

    added animation

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,11 +4,11 @@ const CategoryItem = ({ label, size, imgUrl }) => {
     <div
       className={`${
         size === 'lg' ? 'col-span-3 py-24' : 'col-span-2 py-14'
-      } shadow flex justify-center rounded-md bg-cover bg-center duration-200 hover:shadow-xl`}
+      } flex justify-center rounded-md transition-[background-size] bg-[length:100%] bg-center ease-in-out shadow duration-[1500ms] hover:bg-[length:150%]`}
       style={{ backgroundImage: `url(${imgUrl})` }}
     >
       <button className='shadow px-8 py-4 text-center rounded-md bg-neutral-50 opacity-85 duration-200 hover:opacity-95 hover:shadow-lg'>
-        <h2>{label}</h2>
+        <h2 className='text-2xl'>{label}</h2>
         <p>Shop Now</p>
       </button>
     </div>
