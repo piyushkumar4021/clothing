d1525375041110bb995a86d6c1cda6d08ba82377
unknown <piyushkumar4075@gmail.com>
2024-10-14 15:21:44 +0530

    create a login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import Shop from './page/Shop';
 import Header from './components/Header';
+import Login from './page/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/'>
           <Route index Component={Home} />
           <Route path='shop' Component={Shop} />
+          <Route path='login' Component={Login} />
         </Route>
       </Routes>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
 
   return (
     <div className='px-6 py-3 mb-4 shadow flex justify-between items-center'>
-      <Link to=''>
-        <img className='w-16' src={logo} alt='Fashion Website Logo' />
+      <Link to='/'>
+        <img className='w-12 sm:w-16' src={logo} alt='Fashion Website Logo' />
       </Link>
 
-      <div className='text-xl tracking-wider text-neutral-800 flex gap-x-6'>
+      <div className='text-lg tracking-wider text-neutral-800 flex gap-x-6'>
         <NavLink
           to=''
           className={({ isActive }) => (isActive ? activeClass : '')}
@@ -22,6 +22,12 @@ const Header = () => {
           className={({ isActive }) => (isActive ? activeClass : '')}
         >
           Shop
+        </NavLink>
+        <NavLink
+          to='login'
+          className={({ isActive }) => (isActive ? activeClass : '')}
+        >
+          Login
         </NavLink>
       </div>
     </div>
